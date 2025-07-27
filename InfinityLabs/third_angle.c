26bd2d922985901f3ab3963bf746ce2f7c0109ff
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-27 22:26:14 +0300

    ThirdAngle.c
    Given 2 positive integers
    representing, in degrees, 2 angles of a triangle
    returns the third angle.
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/third_angle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int ThirdAngle(int angle1, int angle2)
+{
+    return 180 - angle1 - angle2;
+}
+
+int main()
+{
+    printf("Testing Report:\n");
+    printf("Running test: ThirdAngle(8, 5): %d\n",
+           ThirdAngle(8, 5));
+    return 0;
+}
