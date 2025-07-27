a1eb770505abc62be4c398a6d0a8096fccd64fd3
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-27 22:09:15 +0300

    BoxVolume - returns the volume of the box.
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/box_valume.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int BoxVolume(int length, int width, int height)
+{
+    return length * width * height;
+}
+
+int main()
+{
+    printf("Testing Report:\n");
+    printf("Running test: BoxVolume(length=8, width=5, height=12): %d\n",
+           BoxVolume(8, 5, 12));
+    return 0;
+}
