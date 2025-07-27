5043d2c7a32ee5a00ca9b7b29578ae995f4a4f65
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-27 22:33:31 +0300

    CirclePerimeter
    a function that is given the radius of a circle and
    returns the perimeter of the circle.
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/circle_perimeter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+const float PI = 3.14;
+
+float CirclePerimeter(float radius)
+{
+    return 2 * radius * PI;
+}
+
+int main()
+{
+    printf("Testing Report:\n");
+    printf("Running test: CirclePerimeter(5.8): %f\n",
+           CirclePerimeter(5.8));
+    return 0;
+}
