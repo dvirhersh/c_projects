bd1baf7528477a0dd30fcbf3be85c06578102272
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-28 22:56:50 +0300

    ReverseNumber
    given a number between 100 and 999
    returns the number in reverse.
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/vairables_n_expressions/ReverseNumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned ReverseNumber(unsigned number)
+{
+    unsigned ones_digit, tens_digit, hundreds_digit, result;
+
+    ones_digit = number % 10;
+    tens_digit = (number / 10) % 10;
+    hundreds_digit = number / 100;
+
+    result = ones_digit * 100 + tens_digit * 10 + hundreds_digit;
+
+    return result;
+}
+
+int main()
+{
+    unsigned number = 321;
+
+    printf("in celsius = %d\n", ReverseNumber(number));
+
+    return 0;
+}
