1aaba798c37cdd2d57175f32be3dc14dd8080354
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-28 23:45:58 +0300

    TriangleType
    If the triangle is right, isosceles, or both,
    the function should return 1, 2 or 3, respectively.
    Otherwise, for a valid triangle, it shoud return 0.
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/conditional_execution/triangle_type.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int TriangleType(unsigned angle1, unsigned angle2)
+{
+    unsigned angle3 = 180 - (angle1 + angle2);
+    int result = 0;
+
+    if (angle1 == 0 || angle2 == 0 || angle1 + angle2 >= 180)
+        return -1;
+
+    if (angle1 == 90 || angle2 == 90 || angle3 == 90)
+        result++;
+
+    if (angle1 == angle2 || angle1 == angle3 || angle2 == angle3)
+        result += 2;
+
+    return result;
+}
+
+int main()
+{
+    printf("the triangle kind is: %d\n", TriangleType(-1, 2));
+
+    return 0;
+}
