0e2cfd0a3e9447c45a61e5eb8c80cb332e2e5f5f
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-29 14:36:51 +0300

    Fibonacci
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/loop_statements/fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+
+unsigned Fibonacci(unsigned index)
+{
+    unsigned i = 1, fib_num = 0, num0 = 0, num1 = 1;
+
+    for (i = 0; i <= index; i++)
+    {
+        fib_num = num0;
+        num0 = num1;
+        num1 += fib_num;
+    }
+
+    return fib_num;
+}
+
+float main()
+{
+    printf("Testing Report:\n");
+    printf("Fibonacci: %d\n", Fibonacci(0));
+    printf("Fibonacci: %d\n", Fibonacci(1));
+    printf("Fibonacci: %d\n", Fibonacci(2));
+    printf("Fibonacci: %d\n", Fibonacci(3));
+    printf("Fibonacci: %d\n", Fibonacci(4));
+    printf("Fibonacci: %d\n", Fibonacci(5));
+    printf("Fibonacci: %d\n", Fibonacci(6));
+    printf("Fibonacci: %d\n", Fibonacci(7));
+    printf("Fibonacci: %d\n", Fibonacci(8));
+    printf("Fibonacci: %d\n", Fibonacci(9));
+    printf("Fibonacci: %d\n", Fibonacci(10));
+
+    return 0;
+}
