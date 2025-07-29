088920b8d4cd7769bb10074bbc7a524ee2a0674d
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-29 11:53:32 +0300

    Maximum
    given 2 integers. It should return the greater one.
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ InfinityLabs/conditional_execution/maximum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Maximum(int num1, int num2)
+{
+    return num1 < num2 ? num2 : num1;
+}
+
+int main()
+{
+    printf("1 The max number is %d\n", Maximum(-1, 90));
+    printf("2 The max number is %d\n", Maximum(85, 5));
+    printf("3 The max number is %d\n", Maximum(70, 70));
+
+    return 0;
+}
