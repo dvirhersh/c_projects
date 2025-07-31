c001892cab99582436554737f443e5af8079988c
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-31 15:33:11 +0300

    RangeSum
    using do-while
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- InfinityLabs/loop_statements/range_sum.c
+++ InfinityLabs/loop_statements/range_sum.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
+// int RangeSum(int start, int end)
+// {
+//     int i, sum = 0;
+//     for (i = start + !(start % 2); i <= end; i += 2)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
 int RangeSum(int start, int end)
 {
-    int i, sum = 0;
-    for (i = start + !(start % 2); i <= end; i += 2)
+    int i = start + !(start % 2), sum = 0;
+
+    do
     {
         sum += i;
-    }
+        i += 2;
+    } while (i <= end);
     return sum;
 }
 
 int main()
 {
-    int start;
-    int end;
+    int start = 6;
+    int end = 17;
 
     printf("Testing Report:\n");
     printf("Enter start:\n");
     scanf("%d", &start);
     printf("Enter end:\n");
-    scanf("%d", &end);
+    // scanf("%d", &end);
 
     printf("Running test: RangeSum(%d, %d):\n %d\n", start, end, RangeSum(start, end));
 
