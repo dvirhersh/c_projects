427d394547d2176945378df4b9d9c79a221efd3a
Dvir Hershkovits <dvirhersh@gmail.com>
2025-08-17 07:50:36 +0300

    print the address itself
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- InfinityLabs/pointer_to_pointer/tryout.c
+++ InfinityLabs/pointer_to_pointer/tryout.c
@@ -7,9 +7,33 @@ int main()
     int *p = &x;   // p points to x
     int **pp = &p; // pp points to p
 
-    printf("x  = %d\n", x);      // 42
-    printf("*p = %d\n", *p);     // 42
-    printf("**pp = %d\n", **pp); // 42
+    printf("x      = %d\n", x);      // value of x
+    printf("*p     = %d\n", *p);     // value at address p
+    printf("**pp   = %d\n\n", **pp); // value at address stored in pp
 
+    printf("&x     = %p\n", (void *)&x);  // address of x
+    printf("p      = %p\n\n", (void *)p); // value of p (address of x)
+
+    printf("size of x  : %d bytes\n", sizeof(x));
+    printf("size of p  : %d bytes\n", sizeof(p));
+    printf("size of pp : %d bytes\n\n", sizeof(pp));
+
+    printf("&p     = %p\n", (void *)&p);    // address of p
+    printf("pp     = %p\n", (void *)pp);    // value of pp (address of p)
+    printf("&pp    = %p\n\n", (void *)&pp); // address of pp
+
+    printf("Size of int:        %zu bytes\n", sizeof(int));
+    printf("Size of int*:       %zu bytes\n", sizeof(int *));
+
+    printf("Size of char:       %zu bytes\n", sizeof(char));
+    printf("Size of char*:      %zu bytes\n", sizeof(char *));
+
+    printf("Size of float:      %zu bytes\n", sizeof(float));
+    printf("Size of float*:     %zu bytes\n", sizeof(float *));
+
+    printf("Size of double:     %zu bytes\n", sizeof(double));
+    printf("Size of double*:    %zu bytes\n", sizeof(double *));
+
+    printf("Size of void*:      %zu bytes\n", sizeof(void *));
     return 0;
 }
