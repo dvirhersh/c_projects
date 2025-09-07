41e17a54326ba69591cacd381bd52f64e7b2a1be
dvir.hershkovits <dvirhersh@gmail.com>
2025-09-07 17:02:54 +0300

    func receives an int and flips the order of the digits

---
+++ c/ws1/ex5_flip_digits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int flip_digits(int n)
+{
+    int fliped_number = 0;
+
+    while (0 != n)
+    {
+        int digit = n % 10;
+        fliped_number = fliped_number * 10 + digit;
+        n /= 10;
+    }
+    return fliped_number;
+}
+
+int main(void)
+{
+    int n = 45920;
+    int fliped_number = flip_digits(n);
+    printf("number %d flipped to %d\n", n, fliped_number);
+    return 0;
+}
