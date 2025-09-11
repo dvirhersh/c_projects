a9128a3c7da9d4521e27384d8bfee3010edc8920
dvir.hershkovits <dvirhersh@gmail.com>
2025-09-11 16:52:48 +0300

    buffer with lower case

--- c/array_pointer_2_pointer/src/buffer_lower_case.c
+++ c/array_pointer_2_pointer/src/buffer_lower_case.c
@@ -1,6 +1,75 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
-char **BufferLowerCase()
+static int NumOfEnvArgs(char *envp[]);
+static char** CopyEnvArgsToBuffLower(char *envp[], size_t num_of_env_args);
+static char* LowerCase(const char* ch);
+
+char** BufferLowerCase(char *envp[])
 {
-    
+    int num_of_env_args = NumOfEnvArgs(envp);
+    char** buff_lower_case = CopyEnvArgsToBuffLower(envp, num_of_env_args);
+    assert(buff_lower_case);
+    return buff_lower_case;
+}
+
+static int NumOfEnvArgs(char *envp[])
+{
+    int num_of_env_args = 0;
+    int i = 0;
+    for (; envp[i] != NULL; i++)
+        num_of_env_args++;
+    return num_of_env_args;
+}
+
+static char** CopyEnvArgsToBuffLower(char *envp[], size_t num_of_env_args)
+{
+    size_t i = 0;
+    char **buff = (char **)malloc((num_of_env_args + 1) * sizeof(char*));
+    assert(buff);
+
+    for (; i < num_of_env_args; i++)
+        buff[i] = LowerCase(envp[i]);
+    buff[num_of_env_args] = NULL;
+
+    return buff;
+}
+
+static char* LowerCase(const char* str)
+{
+    size_t i = 0;
+    size_t len = strlen(str);
+    char* str_lower_case = (char*)malloc(len + 1);
+    assert(str_lower_case);
+
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str_lower_case[i] = str[i] + 'a' - 'A';
+        else
+            str_lower_case[i] = str[i];
+        str_lower_case[len] = '\0';
+    }
+
+    return str_lower_case;
+}
+
+void PrintBuffer(char** ch)
+{
+    int i = 0;
+    for (; ch[i] != NULL; i++)
+    {
+        printf("%s\n", ch[i]);
+    }
+}
+
+void FreeBuffer(char **ch)
+{
+    int i = 0;
+    for (i = 0; ch[i] != NULL; i++)
+    {
+        free(ch[i]);
+    }
 }
--- c/array_pointer_2_pointer/test/array_pointer_2_pointer_test.c
+++ c/array_pointer_2_pointer/test/array_pointer_2_pointer_test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "matrix_sum_of_each_row.h"
 #include "joseph_problem.h"
 #include "data_type.h"
+#include "buffer_lower_case.h"
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[], char **envp)
 {
 
     int matrix[3][4] =
@@ -13,33 +15,23 @@ int main(int argc, char *argv[], char *envp[])
 
     int result[3] = {0};
     size_t i = 0;
-    int j = 0;
+    char** buffer_lower_case = BufferLowerCase(envp);
+    PrintBuffer(buffer_lower_case);
+    FreeBuffer(buffer_lower_case);
+    free(buffer_lower_case);
 
-    printf("matrix[1][3] = %d\n", matrix[1][3]);
+    printf("\nmatrix[1][3] = %d\n", matrix[1][3]);
 
     MatrixSumOfEachRow(3, 4, (const int*)matrix, result);
 
-    printf("Test array and pointer to pointer ->\n\n");
+    printf("\nTest array and pointer to pointer ->\n\n");
 
-    for (; i < 3; i++)
+    for (i = 0; i < 3; i++)
     {
         printf("result arr[%lu] is: %d \n", i, result[i]);
     }
 
     DataTypes();
-
-
-    printf("Dvir debug 1 :\n");
-    printf("Number of arguments: %d\n", argc);
-
-    for (j = 0; j < argc; i++) {
-        printf("Argument %lu: %s\n", i, argv[i]);
-    }
-
-    printf("Environment variables:\n");
-    for (i = 0; envp[i] != NULL; i++) {
-        printf("  %s\n", envp[i]);
-    }
 
     return 0;
 }
