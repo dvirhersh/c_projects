f87851bd238311e3f43d7f47f839f0c532704bd9
dvir.hershkovits <dvirhersh@gmail.com>
2025-09-14 17:27:14 +0300

    delete uncessary files

--- c/Files/stdout_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int result = 42;
-    // The fprintf function is used to print the formatted output to stdout.
-    fprintf(stdout, "Result: %d\n", result);
-    printf("stdout = %p\n", (void *)stdout);
-    return 0;
-}
--- c/Files/tty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Enter your name:" > /dev/tty
-read name < /dev/tty
-echo "Hello, $name!"
